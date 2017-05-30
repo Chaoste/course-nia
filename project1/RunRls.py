d190b800b81910cc4287344418bd4f2d60c112c9
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-05-30 22:51:58 +0200

    Add filename suffix for storage, run scripts with only_greater=True

--- project1/RunRls.py
+++ project1/RunRls.py
@@ -4,8 +4,8 @@ from manualScript import *
 
 def main():
     algorithms = (rls,)
-    problems = (one_max, leading_ones, jump, royal_roads, bin_val)
-    evaluate_all(np.arange(25, 201, 25), algorithms, problems, only_greater=False)
+    problems = (one_max, leading_ones)
+    evaluate_all(np.arange(25, 201, 25), algorithms, problems, only_greater=True, filename_suffix=" - RI")
 
 if __name__ == "__main__":
     main()
