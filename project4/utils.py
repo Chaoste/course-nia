11bd211619b96bb612e351896781567a53b5c877
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-07-22 16:47:51 +0200

    Add multi object metric, calculate for teaming1, clean up notebook

--- project4/utils.py
+++ project4/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import csv
 
 def process_semesters(students, target_func):
-    result = pd.DataFrame(columns=['hash', 'team', 'Semester'])
+    result = pd.DataFrame(columns=['hash', 'Team', 'Semester', 'Sex', 'Discipline', 'Nationality'])
     for semester in ('WT-15', 'ST-16', 'WT-16', 'ST-17'):
         sem_students = students[students['Semester'] == semester]
         assert len(sem_students) in (80, 81),\
@@ -14,7 +14,13 @@ def process_semesters(students, target_func):
 
 def store_teaming(teaming, filename=None):
     # Use the same format as used for the input data
-    return teaming.to_csv(filename, quoting=csv.QUOTE_ALL, index=False)
+    return teaming.to_csv(filename, quoting=csv.QUOTE_ALL, index=False,
+                          columns=['hash', 'Team', 'Semester'])
+
+def print_metric(metric_output, teaming_name):
+    print('Multi-objective metric for {}: '.format(teaming_name) +
+          'GenderBalance={:.2f}, Disciplines={:.2f}'.format(*metric_output[:2]) +
+          ', Nationalities={:.2f}, Collision={:.2f}'.format(*metric_output[2:]))
 
 #------------------------------------------------------------------------------#
 # Teaming algorithms
@@ -24,9 +30,11 @@ def arbitrary_teaming(students, semester):
     groups = []
     for i in range(15):
         team = students[i*5:(i+1)*5]
-        groups.extend((hash_, i+1, semester) for hash_ in team['hash'])
-    groups.extend((hash_, 16, semester) for hash_ in students[75:]['hash'])
-    grouped = pd.DataFrame(groups, columns=['hash', 'team', 'Semester'])
+        groups.extend((x['hash'], i+1, x['Semester'], x['Sex'], x['Discipline'], x['Nationality'])
+                      for _, x in team.iterrows())
+    groups.extend((x['hash'], 16, x['Semester'], x['Sex'], x['Discipline'], x['Nationality'])
+                  for _, x  in students[75:].iterrows())
+    grouped = pd.DataFrame(groups, columns=['hash', 'Team', 'Semester', 'Sex', 'Discipline', 'Nationality'])
     return grouped
 
 def intra_diversity_teaming(students, semester):
